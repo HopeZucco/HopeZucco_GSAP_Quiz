896e3364639f541c468401cebf8ac14714bdccd6
HopeZucco <hmz34@drexel.edu>
2022-04-14 19:54:59 -0400

    challenge 1

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,7 +1,21 @@
-//import { gsap } from "gsap";
+import { gsap } from "gsap";
 
 //Replace this value with your name
-const yourName = "Your Name"
+const yourName = "Hope Zucco";
 document.querySelector("#name").innerHTML = yourName;
 
 //GreenSock Timeline
+const mainTL = gsap.timeline();
+
+
+//Part 1
+mainTL
+.from("#red", {x: 0, duration: 1 })
+.from("#blue", {x: 350, duration: 1})
+.to("#red", {y: 0, duration: 2})
+.to("#blue", {y: 300, duration: 2})
+
+
+//Part 2
+//.from
+;
